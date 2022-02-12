c4dfb2a765f7f6e9f4437d262940527f17bf3e25
Juan Trejos <jctrejosi@unal.edu.co>
2022-02-12 01:02:25 -0500

    Create Header component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>Initial Page</div>
+  <Header />
 </template>
+
+<script>
+import Header from '@/components/Header/Header.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+html, body {
+  height: 100vh;
+  margin: 0;
+  width: 100vw;
+}
+</style>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="header">
+    <div class="header__title">Mascotitas</div>
+    <div class="header__icon">
+      <leaf />
+    </div>
+  </div>
+</template>
+
+<script>
+import leaf from '@/assets/icons/leaf.vue'
+
+export default {
+  name: 'Header',
+
+  components: {
+    leaf
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  align-items: center;
+  background-color: $main-color;
+  display: flex;
+  font-family: $main-font;
+  height: 2rem;
+  justify-content: center;
+  left: 0;
+  max-width: 100%;
+  padding: 1rem 2rem;
+  position: relative;
+  top: 0;
+
+  &__title {
+    color: $custom-color;
+    font-size: $main-title-size;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+
+  &__icon {
+    padding: 1rem;
+    fill: $custom-color;
+  }
+}
+</style>
